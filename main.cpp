7346b78c83face82b512833eef688a336fa26a22
JustRayCB <rayancbpro@gmail.com>
2022-11-18 09:46:32 +0100

    test

--- main.cpp
+++ main.cpp
@@ -80,7 +80,7 @@ Do not edit!!!!
 
 
 int main(int argc, char *argv[]) {
-    cout << "Yoo mon reuf" << endl;
+    cout << "Yoo mon reuf haha " << endl;
     srand(time(0));
     MainWindow window;
     window.show(argc, argv);
