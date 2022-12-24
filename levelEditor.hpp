f8fd2baf0f99629e59fbc58a54fab27c72b6e61e
callhc <hugo.callens@ulb.be>
2022-12-24 13:30:35 +0100

    testing new window

--- levelEditor.hpp
+++ levelEditor.hpp
@@ -18,50 +18,63 @@
 #include <Fl/Fl_Choice.H>
 #include <memory.h>
 
-void test_callback(Fl_Widget*, void *data){
-    //std::cout << colInput << std::endl;
-    //std::cout << lineInput << std::endl;
-    std::cout << "test" << std::endl;
-}   
-
 class LevelEditorWindow : public Fl_Window {
+private:
     Fl_Button* submitButton;
     Fl_Input* lineInput;
     Fl_Input* colInput;
+    void bouton_callback();
+    static void static_bouton_callback(Fl_Widget* w, void* ptr);
 
  public:
+    LevelEditorWindow();
     ~LevelEditorWindow(){
         std::cout << "Destroying LevelEditorWindow" << std::endl;
         delete submitButton;
         delete lineInput;
         delete colInput;
     }
-    LevelEditorWindow() : Fl_Window(000, 000, 1000, 975, "Level Editor") {
-        std::cout << "Welcome" << std::endl;
-        colInput = new Fl_Input (150, 10, 50, 20, "Number of columns: ");
-        lineInput = new Fl_Input (150, 30, 50, 20, "Number of lines: ");
-        submitButton = new Fl_Button(150, 50, 50, 20, "Submit");
-        colInput->show(); lineInput->show(); submitButton->show();
-        submitButton->callback(test_callback);
-    }
-
-    int handle(int event) override {
-        switch(event) {
-            case FL_PUSH:{
-                std::cout << "PUSHING" << std::endl;
-                std::cout << Fl::event_x() << submitButton->x() << std::endl;
-                if (Fl::event_x() <= submitButton->x()+submitButton->w() and Fl::event_x() >= submitButton->x()
-                        and Fl::event_y() <= submitButton->y()+submitButton->h() and Fl::event_y() >= submitButton->y()) {
-                    std::cout << lineInput->value() << std::endl;
-                    std::cout << colInput->value() << std::endl;
-                    std::cout << "IM" << std::endl;
-                }
-                return 1;
-            }
-        }
-        return 0;
-    }
 };
+
+    // int handle(int event) override {
+    //     switch(event) {
+    //         case FL_PUSH:{
+    //             std::cout << "PUSHING" << std::endl;
+    //             std::cout << Fl::event_x() << submitButton->x() << std::endl;
+    //             if (Fl::event_x() <= submitButton->x()+submitButton->w() and Fl::event_x() >= submitButton->x()
+    //                     and Fl::event_y() <= submitButton->y()+submitButton->h() and Fl::event_y() >= submitButton->y()) {
+    //                 std::cout << lineInput->value() << std::endl;
+    //                 std::cout << colInput->value() << std::endl;
+    //                 std::cout << "IM" << std::endl;
+    //             }
+    //             if (Fl::event_x() <= colInput->x()+colInput->w() and Fl::event_x() >= colInput->x()
+    //                     and Fl::event_y() <= colInput->y()+colInput->h() and Fl::event_y() >= colInput->y()) {
+    //                 colInput
+    //             }
+    //             return 1;
+    //         }
+    //     }
+    //     return 0;
+    // }
+void LevelEditorWindow::bouton_callback(){
+    printf("Hi");
+}
+void LevelEditorWindow::static_bouton_callback(Fl_Widget* w, void* ptr){
+    LevelEditorWindow* me = static_cast<LevelEditorWindow*>(ptr);
+    me->bouton_callback();
+}
+
+
+LevelEditorWindow::LevelEditorWindow() : Fl_Window(000, 000, 1000, 975, "Level Editor"){
+    std::cout << "Welcome" << std::endl;
+    colInput = new Fl_Input (150, 10, 50, 20, "Number of columns: ");
+    lineInput = new Fl_Input (150, 30, 50, 20, "Number of lines: ");
+    submitButton = new Fl_Button(150, 50, 50, 20, "Submit");
+    //colInput->show(); lineInput->show(); submitButton->show();
+    submitButton->callback(static_bouton_callback, this);
+    end();
+    show();
+}
 
 void generateButtonEditor();
 #endif
