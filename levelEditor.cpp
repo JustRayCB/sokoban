ec74e1bd67551d3cd2c845e078f476dac89564f8
JustRayCB <rayancbpro@gmail.com>
2022-12-24 17:05:12 +0100

    Fix level Edito header

--- levelEditor.cpp
+++ levelEditor.cpp
@@ -5,3 +5,20 @@ void generateButtonEditor(){
     LevelEditorWindow* editor = new LevelEditorWindow;
     editor->show();
 }
+void LevelEditorWindow::bouton_callback(){
+    printf("Hi");
+}
+void LevelEditorWindow::static_bouton_callback(Fl_Widget* w, void* ptr){
+    LevelEditorWindow* me = static_cast<LevelEditorWindow*>(ptr);
+    me->bouton_callback();
+}
+LevelEditorWindow::LevelEditorWindow() : Fl_Window(000, 000, 1000, 975, "Level Editor"){
+    std::cout << "Welcome" << std::endl;
+    colInput = new Fl_Input (150, 10, 50, 20, "Number of columns: ");
+    lineInput = new Fl_Input (150, 30, 50, 20, "Number of lines: ");
+    submitButton = new Fl_Button(150, 50, 50, 20, "Submit");
+    //colInput->show(); lineInput->show(); submitButton->show();
+    submitButton->callback(static_bouton_callback, this);
+    end();
+    show();
+}
--- levelEditor.hpp
+++ levelEditor.hpp
@@ -27,6 +27,7 @@ private:
     static void static_bouton_callback(Fl_Widget* w, void* ptr);
 
  public:
+    //LevelEditorWindow() : Fl_Window(000, 000, 1000, 975, "Level Editor");
     LevelEditorWindow();
     ~LevelEditorWindow(){
         std::cout << "Destroying LevelEditorWindow" << std::endl;
@@ -56,25 +57,8 @@ private:
     //     }
     //     return 0;
     // }
-void LevelEditorWindow::bouton_callback(){
-    printf("Hi");
-}
-void LevelEditorWindow::static_bouton_callback(Fl_Widget* w, void* ptr){
-    LevelEditorWindow* me = static_cast<LevelEditorWindow*>(ptr);
-    me->bouton_callback();
-}
 
 
-LevelEditorWindow::LevelEditorWindow() : Fl_Window(000, 000, 1000, 975, "Level Editor"){
-    std::cout << "Welcome" << std::endl;
-    colInput = new Fl_Input (150, 10, 50, 20, "Number of columns: ");
-    lineInput = new Fl_Input (150, 30, 50, 20, "Number of lines: ");
-    submitButton = new Fl_Button(150, 50, 50, 20, "Submit");
-    //colInput->show(); lineInput->show(); submitButton->show();
-    submitButton->callback(static_bouton_callback, this);
-    end();
-    show();
-}
 
 void generateButtonEditor();
 #endif
